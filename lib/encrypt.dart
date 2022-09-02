6f1ecebc7306b30a7d1d29d7bbe7fabe7a4a55be
Tukhtamurodov-Sardorbek <sardortukhtamurodov02@gmail.com>
2022-09-02 18:59:07 +0500

    Added Fernet Algorithm

--- lib/encrypt.dart
+++ lib/encrypt.dart
@@ -1,12 +1,14 @@
 import 'package:encrypt/encrypt.dart' as encrypt;
 
 class EncryptDecrypt{
-  static final key = encrypt.Key.fromLength(32);
-  static final iv = encrypt.IV.fromLength(16);
-  static final encrypter = encrypt.Encrypter(encrypt.AES(key));
+  
+  /// AES Algorithm
+  static final keyAES = encrypt.Key.fromLength(32);
+  static final ivAES = encrypt.IV.fromLength(16);
+  static final encrypterAES = encrypt.Encrypter(encrypt.AES(keyAES));
 
   static EncryptAES(String text){
-    final encrypted = encrypter.encrypt(text, iv: iv);
+    final encrypted = encrypterAES.encrypt(text, iv: ivAES);
 
     print('**********************************');
     print('****   BYTES: ${encrypted.bytes}');
@@ -16,9 +18,28 @@ class EncryptDecrypt{
 
     return encrypted;
   }
-
   static DecryptAES(encrypt.Encrypted encrypted){
-    final decrypted = encrypter.decrypt(encrypted, iv: iv);
+    final decrypted = encrypterAES.decrypt(encrypted, iv: ivAES);
+    return decrypted;
+  }
+
+  /// Fernet Algorithm
+  static final keyFernet = encrypt.Key.fromUtf8('anyKeyButItMustBe32ByteCharacter');
+  static final encrypterFernet = encrypt.Encrypter(encrypt.Fernet(keyFernet));
+
+  static EncryptFernet(String text){
+    final encrypted = encrypterFernet.encrypt(text);
+
+    print('**********************************');
+    print('****   BYTES: ${encrypted.bytes}');
+    print('****   BASE16: ${encrypted.base16}');
+    print('****   BASE64: ${encrypted.base64}');
+    print('**********************************');
+
+    return encrypted;
+  }
+  static DecryptFernet(encrypt.Encrypted encrypted){
+    final decrypted = encrypterFernet.decrypt(encrypted);
     return decrypted;
   }
 }
---
+++ lib/home.dart
@@ -0,0 +1,173 @@
+import 'package:flutter/material.dart';
+import 'package:encryption_decryption/encrypt.dart';
+import 'package:encrypt/encrypt.dart' as encrypt;
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  TextEditingController txtController = TextEditingController();
+  String? plainText;
+  String? currentAlgorithm = 'AES';
+  var encryptedText;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Encryption ~ Decryption'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: Column(
+          children: [
+            DropdownButtonFormField(
+              value: currentAlgorithm,
+              items: const [
+                DropdownMenuItem(value: 'AES', child: Text('AES Algorithm')),
+                DropdownMenuItem(
+                    value: 'Fernet', child: Text('Fernet Algorithm')),
+                DropdownMenuItem(
+                    value: 'Salsa20', child: Text('Salsa20 Algorithm')),
+              ],
+              decoration: const InputDecoration(
+                border: InputBorder.none,
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.amberAccent, width: 3),
+                  borderRadius: BorderRadius.all(Radius.circular(12.0)),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.amberAccent, width: 3),
+                  borderRadius: BorderRadius.all(Radius.circular(12.0)),
+                ),
+              ),
+              onChanged: (String? value) {
+                setState(() {
+                  currentAlgorithm = value;
+                });
+              },
+            ),
+            const SizedBox(height: 24.0),
+            TextField(
+              controller: txtController,
+              decoration: const InputDecoration(
+                border: InputBorder.none,
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.amberAccent, width: 3),
+                  borderRadius: BorderRadius.all(Radius.circular(12.0)),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.amberAccent, width: 3),
+                  borderRadius: BorderRadius.all(Radius.circular(12.0)),
+                ),
+              ),
+            ),
+            const SizedBox(height: 24.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    plainText = txtController.text.trim();
+                    if(currentAlgorithm == 'AES'){
+                      encryptedText = EncryptDecrypt.EncryptAES(plainText!);
+                    } else if(currentAlgorithm == 'Fernet'){
+                      encryptedText = EncryptDecrypt.EncryptFernet(plainText!);
+                    }
+                    setState(() {});
+                  },
+                  child: const Text('Encrypt'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    if (encryptedText is encrypt.Encrypted) {
+                      if(currentAlgorithm == 'AES'){
+                        encryptedText = EncryptDecrypt.DecryptAES(encryptedText);
+                      } else if(currentAlgorithm == 'Fernet'){
+                        encryptedText = EncryptDecrypt.DecryptFernet(encryptedText);
+                      }
+                      setState(() {});
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary: encryptedText is encrypt.Encrypted
+                        ? Colors.amber
+                        : Colors.amberAccent.shade100,
+                  ),
+                  child: Text(
+                    'Decrypt',
+                    style: TextStyle(
+                      color: encryptedText is encrypt.Encrypted
+                          ? Colors.black
+                          : Colors.black45,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 30.0),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      'PLAIN TEXT',
+                      style: TextStyle(
+                        fontSize: 22,
+                        color: Colors.amber[400],
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    const SizedBox(height: 12.0),
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      padding: const EdgeInsets.all(12.0),
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.amberAccent, width: 3),
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(12.0)),
+                      ),
+                      child: Text(plainText ?? ''),
+                    ),
+                    const SizedBox(height: 24.0),
+                    Text(
+                      'ENCRYPTED TEXT',
+                      style: TextStyle(
+                        fontSize: 22,
+                        color: Colors.amber[400],
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    const SizedBox(height: 12.0),
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      padding: const EdgeInsets.all(12.0),
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.amberAccent, width: 3),
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(12.0)),
+                      ),
+                      child: Text(
+                          encryptedText == null
+                          ? ''
+                          : encryptedText is encrypt.Encrypted
+                              ? encryptedText.base64
+                              : encryptedText,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
